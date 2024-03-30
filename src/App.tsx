25dab789f74947554b3787b203988004dbef9147
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-03-29 21:10:16 -0600

    Ahora si arreglado lo de que el app reconozca al provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,16 @@
 import './App.css'
 import Stepper from './quiz02/Stepper'
 import Content from './quiz02/Content'
-import { MyProvider } from './context/MyContextProvider'
 import { useContext } from 'react'
 import MyContext from './context/MyContext'
 
 
 
 function App() {
-  return (
-    <MyProvider>
-      <AppComponent />
-    </MyProvider>
-  );
-}
-
-function AppComponent() {
   const { step, setStep } = useContext(MyContext);
-
   return (
     <>
-      <h1>Quiz 02</h1>
+     <h1>Quiz 02</h1>
       <Stepper/>
       <Content/>
       <div className='flex'>
@@ -31,5 +21,6 @@ function AppComponent() {
     </>
   );
 }
+
 
 export default App;
